40c946559b84320ac74d7d059efb6854a54573a5
lawson11234-sl <lawson11234@gmail.com>
2026-05-14 20:00:16 +0800

    Add Firebase database URL

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyA4-UMqk70pTNIAUQci75uDTIiagjyi3UI",
+  authDomain: "shaizi-c5bbc.firebaseapp.com",
+  databaseURL: "https://shaizi-c5bbc-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "shaizi-c5bbc",
+  storageBucket: "shaizi-c5bbc.firebasestorage.app",
+  messagingSenderId: "724269795514",
+  appId: "1:724269795514:web:990fb0b778bc1eed759a83",
+  measurementId: "G-PER8259VCR"
 };
